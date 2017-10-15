db6cafae567875039adc2a017da206e66efbf0e3
azimprad <azimprad@amazon.com>
2017-10-15 08:43:43 -0700

    Create api starter project

---
+++ tests/handler.spec.js
@@ -0,0 +1,10 @@
+import hello from '../services/handler';
+
+test('The default handler should work', () => {
+  const event = {};
+  const context = {};
+  const callback = (ctx, data) => {
+    console.log(data);
+  };
+  hello(event, context, callback);
+});
